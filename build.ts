e18efae06c8e8d05f4f7405cb0b2679fb068070b
zeref09 <36810509+zeref09@users.noreply.github.com>
2022-09-05 23:27:30 +1000

    Removed dotfuscator calls

--- build.ts
+++ build.ts
@@ -1,7 +1,6 @@
 import * as child_process from "child_process";
 import { getMsBuildPath, deleteDirectoryRecursive } from "metame-common";
-import * as path from "path";
-import { getDotfuscatorCLIPath, getDistDirPath } from "./path";
+import { getDistDirPath } from "./path";
 import * as fs from "fs-extra";
 
 async function main() {
@@ -16,26 +15,11 @@ async function main() {
   let cmd: string = `"${msBuildPath}" ${solutionPath} /p:Configuration=Release /t:Rebuild`;
   child_process.execSync(cmd, { stdio: [0, 1, 2] });
 
-  console.log("Obfuscating...");
-  runDotfuscator();
-
   console.log("Copying to dist...");
   copyToDist();
 }
 
 main();
-
-function runDotfuscator() {
-  //obfuscate code dlls
-  //config moves from release folder > Dotfuscator folder
-  const configPath: string = `${__dirname}\\Dotfuscator.xml`;
-  var dotfuscatorPath = getDotfuscatorCLIPath();;
-  if (!dotfuscatorPath) {
-    throw 'dotfuscator.exe not found';
-  }
-  let cmd: string = `"${dotfuscatorPath}" ${configPath}`;
-  child_process.execSync(cmd, { stdio: [0, 1, 2] });
-}
 
 async function copyToDist() {
   const distPath: string = getDistDirPath();
@@ -48,21 +32,4 @@ async function copyToDist() {
   //copy release folder
   const targetDir: string = `${__dirname}\\metame-service\\bin\\Release`;
   await fs.copy(targetDir, distPath);
-
-  //copy over non .xml files
-  console.log("Copy Dotfuscated folder to dist...");
-  const dotfuscatorDir: string = path.join(__dirname, "Dotfuscated");
-  var obfuscatedFiles: string[] = await fs.readdir(dotfuscatorDir);
-  var relevantFiles = obfuscatedFiles.filter((fileName) => {
-    return path.extname(fileName) != ".xml";
-  });
-
-  relevantFiles.forEach((fileName) => {
-    var src = path.join(dotfuscatorDir, fileName);
-    var dest = path.join(distPath, fileName);
-    console.log(`Copying ${src} > ${dest}`);
-    fs.copySync(src, dest);
-  });
-
 }
-
