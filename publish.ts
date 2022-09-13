29fbaa71a638fa01f9811fb495b3576c0b26b75e
zeref09 <36810509+zeref09@users.noreply.github.com>
2022-09-13 23:00:47 +1000

    Added dotenv

--- publish.ts
+++ publish.ts
@@ -2,25 +2,27 @@ import * as child_process from "child_process";
 import * as commonLib from "metame-common";
 import * as path from "path";
 import { getDistDirPath } from "./path";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
 async function main() {
   var version = require("./package.json").version;
   //copy binaries in compiled into bin
-  const targetDir = getDistDirPath(); 
+  const targetDir = getDistDirPath();
   const destDir = `${__dirname}\\metame-service-win-release\\bin`;
 
-  let fileNames: Array<string> = ["MetaMe.WindowsClient.exe", "MetaMe.Sensors.dll", "MetaMe.Core.dll"];
+  // let fileNames: Array<string> = ["MetaMe.WindowsClient.exe", "MetaMe.Sensors.dll", "MetaMe.Core.dll"];
 
-  //now sign the files
-  var signToolExe = commonLib.getSignToolPath();
-  let signPaths: Array<string> = fileNames.map(fileName => path.join(targetDir, fileName));
-  for (let path of signPaths) {
-    console.log(`Signing ${path}...`);
-    // var signEngineCmd = `"${signToolExe}" sign /tr http://timestamp.comodoca.com /td sha256 /fd sha256 /a /v "${path}"`;
-    var signEngineCmd = `"${signToolExe}" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a /v "${path}"`;
-    await commonLib.executeCommand(signEngineCmd);
-  }
+  // //now sign the files
+  // var signToolExe = commonLib.getSignToolPath();
+  // let signPaths: Array<string> = fileNames.map(fileName => path.join(targetDir, fileName));
+  // for (let path of signPaths) {
+  //   console.log(`Signing ${path}...`);
+  //   // var signEngineCmd = `"${signToolExe}" sign /tr http://timestamp.comodoca.com /td sha256 /fd sha256 /a /v "${path}"`;
+  //   var signEngineCmd = `"${signToolExe}" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a /v "${path}"`;
+  //   await commonLib.executeCommand(signEngineCmd);
+  // }
 
   console.log(`Publishing version ${version}`);
 
@@ -29,14 +31,12 @@ async function main() {
   commonLib.deleteDirectoryRecursive(clonedPath);
 
   //git clone
-  let publishRepositoryUrl: string =
-    "https://bitbucket.linkenlabs.com/scm/ma/metame-service-win-release.git";
+  let publishRepositoryUrl: string = process.env.PUBLISH_REPO;
+  console.log(`Using repository: ${publishRepositoryUrl}`);
   var gitCloneCmd = `git clone ${publishRepositoryUrl}`;
 
   child_process.execSync(gitCloneCmd, { stdio: [0, 1, 2] });
-  //set git name and email 
-  child_process.execSync(`cd metame-service-win-release && git config user.name "Andrew"`, { stdio: [0, 1, 2] });
-  child_process.execSync(`cd metame-service-win-release && git config user.email "miku23@protonmail.com"`, { stdio: [0, 1, 2] });
+  //set git name and email
 
   //check if tag already exists. npm version prepends a v at the beginning of the version
   var result = await commonLib.executeCommand(
@@ -63,9 +63,11 @@ async function main() {
     { stdio: [0, 1, 2] }
   );
 
+  var remoteBranch = process.env.PUBLISH_REPO_BRANCH;
+
   //set the version and tag via npm version then commit both to devel and the tags
   child_process.execSync(
-    `(cd metame-service-win-release && npm version ${version} && git push origin devel && git push --tags origin devel)`,
+    `(cd metame-service-win-release && npm version ${version} && git push origin ${remoteBranch} && git push --tags origin ${remoteBranch})`,
     { stdio: [0, 1, 2] }
   );
 }
